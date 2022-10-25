4dbfd99dc36f423035370bf6020adb1e8aa76cec
rifky <ee.guy.42@gmail.com>
2022-10-25 13:29:48 +0700

    small fixes to camp card

--- src/components/Camp.js
+++ src/components/Camp.js
@@ -340,17 +340,14 @@ function Camp() {
       </div>
 
       <div className="w-full h-[40vw] flex flex-row justify-center align-middle bg-[#F8F0F8]">
-        <div className="w-[25vw] h-[30vw] flex flex-col place-self-center justify-center align-middle text-[2.5vw] text-[#19103D] font-bold text-left px-[3vw] ">
-          THE NEXT LEVEL EXPERIENCE IS BROUGHT TO YOU AT
-        </div>
         <div className="w-[25vw] h-[30vw] flex flex-col place-self-center justify-center align-middle bg-[#05194A] rounded-3xl mx-[2vw]">
-          <p className="text-[1vw] tracking-widest text-white font-bold">
-            J U N I O R
+          <p className="text-[3.5vw] text-center tracking-widest text-white font-bold pb-[2vw]">
+            JUNIOR
           </p>
-          <p className="text-[3.5vw] text-white tracking-wide font-bold py-[2vw]">
+          {/* <p className="text-[3.5vw] text-white tracking-wide font-bold py-[2vw]">
             SGD$4380
-          </p>
-          <div className="inline-flex flex-row justify-start align-middle px-[3vw] py-[0.5vw]">
+          </p> */}
+          <div className="inline-flex flex-row justify-start align-middle px-[2vw] py-[0.5vw]">
             <svg
               width="1.8vw"
               height="1.8vw"
@@ -364,10 +361,27 @@ function Camp() {
               />
             </svg>
             <p className="text-[1.2vw] pl-[1vw] text-white font-bold">
-              Junior Programs
+              The Independence Program
             </p>
           </div>
-          <div className="inline-flex flex-row justify-start align-middle px-[3vw] py-[0.5vw]">
+          <div className="inline-flex flex-row justify-start align-middle px-[2vw] py-[0.5vw]">
+            <svg
+              width="1.8vw"
+              height="1.8vw"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19.1032 5.9549C19.2056 5.85045 19.3278 5.76747 19.4626 5.71082C19.5975 5.65418 19.7422 5.625 19.8885 5.625C20.0347 5.625 20.1795 5.65418 20.3144 5.71082C20.4492 5.76747 20.5714 5.85045 20.6737 5.9549C21.1027 6.3884 21.1087 7.0889 20.6887 7.5299L11.8192 18.0149C11.7185 18.1255 11.5963 18.2144 11.4601 18.2761C11.3239 18.3378 11.1765 18.3711 11.0269 18.3739C10.8774 18.3767 10.7289 18.349 10.5904 18.2924C10.452 18.2358 10.3265 18.1516 10.2217 18.0449L4.82474 12.5759C4.61659 12.3636 4.5 12.0782 4.5 11.7809C4.5 11.4836 4.61659 11.1982 4.82474 10.9859C4.92711 10.8815 5.04929 10.7985 5.18412 10.7418C5.31896 10.6852 5.46374 10.656 5.60999 10.656C5.75624 10.656 5.90102 10.6852 6.03586 10.7418C6.17069 10.7985 6.29287 10.8815 6.39524 10.9859L10.9732 15.6254L19.0732 5.9879C19.0826 5.97631 19.0926 5.96529 19.1032 5.9549Z"
+                fill="white"
+              />
+            </svg>
+            <p className="text-[1.2vw] pl-[1vw] text-left text-white font-bold">
+              For students 6 to 10 years old
+            </p>
+          </div>
+          <div className="inline-flex flex-row justify-start align-middle px-[2vw] py-[0.5vw]">
             <svg
               width="1.8vw"
               height="1.8vw"
@@ -381,7 +395,7 @@ function Camp() {
               />
             </svg>
             <p className="text-[1.2vw] pl-[1vw] text-white font-bold">
-              Junior is 5 days 4 nights
+              5 days 4 nights
             </p>
           </div>
           <div className="hover:cursor-pointer place-self-center place-items-center rounded-full mt-[3vw] px-[2vw] py-[0.5vw] bg-white text-[#05194A] text-[1.2vw]">
@@ -389,13 +403,13 @@ function Camp() {
           </div>
         </div>
         <div className="w-[25vw] h-[30vw] flex flex-col place-self-center justify-center align-middle bg-[#05194A] rounded-3xl mx-[2vw] ">
-          <p className="text-[1vw] tracking-widest text-white font-bold">
-            S E N I O R
+          <p className="text-[3.5vw] text-center tracking-widest text-white font-bold pb-[2vw]">
+            SENIOR
           </p>
-          <p className="text-[3.5vw] text-white tracking-wide font-bold py-[2vw]">
+          {/* <p className="text-[3.5vw] text-white tracking-wide font-bold py-[2vw]">
             SGD$4380
-          </p>
-          <div className="inline-flex flex-row justify-start align-middle px-[3vw] py-[0.5vw]">
+          </p> */}
+          <div className="inline-flex flex-row justify-start align-middle px-[2vw] py-[0.5vw]">
             <svg
               width="1.8vw"
               height="1.8vw"
@@ -409,10 +423,10 @@ function Camp() {
               />
             </svg>
             <p className="text-[1.2vw] pl-[1vw] text-white font-bold">
-              Senior Programs
+              The Leadership Program
             </p>
           </div>
-          <div className="inline-flex flex-row justify-start align-middle px-[3vw] py-[0.5vw]">
+          <div className="inline-flex flex-row justify-start align-middle px-[2vw] py-[0.5vw]">
             <svg
               width="1.8vw"
               height="1.8vw"
@@ -426,7 +440,24 @@ function Camp() {
               />
             </svg>
             <p className="text-[1.2vw] pl-[1vw] text-white font-bold">
-              Junior is 7 days 6 nights
+              For students 11 to 17 years old
+            </p>
+          </div>
+          <div className="inline-flex flex-row justify-start align-middle px-[2vw] py-[0.5vw]">
+            <svg
+              width="1.8vw"
+              height="1.8vw"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M19.1032 5.9549C19.2056 5.85045 19.3278 5.76747 19.4626 5.71082C19.5975 5.65418 19.7422 5.625 19.8885 5.625C20.0347 5.625 20.1795 5.65418 20.3144 5.71082C20.4492 5.76747 20.5714 5.85045 20.6737 5.9549C21.1027 6.3884 21.1087 7.0889 20.6887 7.5299L11.8192 18.0149C11.7185 18.1255 11.5963 18.2144 11.4601 18.2761C11.3239 18.3378 11.1765 18.3711 11.0269 18.3739C10.8774 18.3767 10.7289 18.349 10.5904 18.2924C10.452 18.2358 10.3265 18.1516 10.2217 18.0449L4.82474 12.5759C4.61659 12.3636 4.5 12.0782 4.5 11.7809C4.5 11.4836 4.61659 11.1982 4.82474 10.9859C4.92711 10.8815 5.04929 10.7985 5.18412 10.7418C5.31896 10.6852 5.46374 10.656 5.60999 10.656C5.75624 10.656 5.90102 10.6852 6.03586 10.7418C6.17069 10.7985 6.29287 10.8815 6.39524 10.9859L10.9732 15.6254L19.0732 5.9879C19.0826 5.97631 19.0926 5.96529 19.1032 5.9549Z"
+                fill="white"
+              />
+            </svg>
+            <p className="text-[1.2vw] pl-[1vw] text-white font-bold">
+              7 days 6 nights
             </p>
           </div>
           <div className="hover:cursor-pointer place-self-center place-items-center rounded-full mt-[3vw] px-[2vw] py-[0.5vw] bg-white text-[#05194A] text-[1.2vw]">
